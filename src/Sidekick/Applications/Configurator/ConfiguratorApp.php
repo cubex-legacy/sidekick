fb024853df6aaf2ec7ffa6ffd89ca4c945a02dc9
Oke Ugwu <oke.ugwu@justdevelop.it>
2013-04-29 12:23:20 +0100

    configuratorApp class

---
+++ src/Sidekick/Applications/Configurator/ConfiguratorApp.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @author  oke.ugwu 
+ */
+
+namespace Sidekick\Applications\Configurator;
+
+
+use Bundl\Debugger\DebuggerBundle;
+use Sidekick\Applications\BaseApp\BaseApp;
+use Sidekick\Applications\Configurator\Controllers\DefaultController;
+
+class ConfiguratorApp extends BaseApp
+{
+  public function name()
+  {
+    return "Configurator";
+  }
+
+  public function description()
+  {
+    return "Config Distribution";
+  }
+
+  public function defaultController()
+  {
+    return new DefaultController();
+  }
+
+  public function getBundles()
+  {
+    return [
+//      new DebuggerBundle()
+    ];
+  }
+
+}

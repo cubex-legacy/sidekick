a6ce4bec8c98763bad28e975694c8d598fa81907
Oke Ugwu <oke.ugwu@justdevelop.it>
2013-05-01 17:22:56 +0100

    EnvironmentList templated view model

---
+++ src/Sidekick/Applications/Configurator/Views/EnvironmentList.php
@@ -0,0 +1,16 @@
+<?php
+/**
+* @author: oke.ugwu
+* Application: 
+*/
+namespace Sidekick\Applications\Configurator\Views;
+
+use Cubex\View\TemplatedViewModel;
+
+class EnvironmentList extends TemplatedViewModel
+{
+  public function __construct($envs)
+  {
+    $this->environments = $envs;
+  }
+}

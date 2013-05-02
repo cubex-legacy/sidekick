62d0f755991a2187000e8d41efcf66b6d1792b21
Oke Ugwu <oke.ugwu@justdevelop.it>
2013-05-02 09:58:43 +0100

    added more structure to class to fix commit rSK55b541133f52

--- src/Sidekick/Applications/Configurator/Views/ProjectList.php
+++ src/Sidekick/Applications/Configurator/Views/ProjectList.php
@@ -1,19 +1,56 @@
 <?php
 /**
-* @author: oke.ugwu
-* Application: 
-*/
+ * @author: oke.ugwu
+ * Application:
+ */
 namespace Sidekick\Applications\Configurator\Views;
 
 use Cubex\View\TemplatedViewModel;
 
 class ProjectList extends TemplatedViewModel
 {
-  public function __construct($viewData)
+  protected $_projects;
+  protected $_subProjects;
+  protected $_configGroups;
+
+  public function setProjects($projects)
   {
-    foreach($viewData as $prop => $data)
+    $this->_projects = $projects;
+    return $this;
+  }
+
+  public function setSubProjects($subProjects)
+  {
+    $this->_subProjects = $subProjects;
+    return $this;
+  }
+
+  public function setConfigGroups($configGroups)
+  {
+    $this->_configGroups = $configGroups;
+    return $this;
+  }
+
+  public function getProjects()
+  {
+    return $this->_projects;
+  }
+
+  public function getSubProjectsCount($projectId)
+  {
+    if(isset($this->_subProjects[$projectId]))
     {
-      $this->{$prop}  = $data;
+      return $this->_subProjects[$projectId];
     }
+    return 0;
+  }
+
+  public function getConfigGroupsCount($projectId)
+  {
+    if(isset($this->_configGroups[$projectId]))
+    {
+      return $this->_configGroups[$projectId];
+    }
+    return 0;
   }
 }

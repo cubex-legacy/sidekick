5514d671e583e6703910a432cb7e6e98260a0142
Oke Ugwu <oke.ugwu@justdevelop.it>
2013-05-02 08:39:29 +0100

    ProjectsApp class with proper naming convention. Fix for commit rSK991fd9bc6d10

---
+++ src/Sidekick/Applications/Projects/ProjectsApp.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @author  oke.ugwu
+ */
+
+namespace Sidekick\Applications\Projects;
+
+use Sidekick\Applications\BaseApp\BaseApp;
+use Sidekick\Applications\Projects\Controllers\DefaultController;
+
+class ProjectsApp extends BaseApp
+{
+  public function defaultController()
+  {
+    return new DefaultController();
+  }
+
+  public function name()
+  {
+    return "Projects";
+  }
+
+  public function description()
+  {
+    return "Projects Manager";
+  }
+}

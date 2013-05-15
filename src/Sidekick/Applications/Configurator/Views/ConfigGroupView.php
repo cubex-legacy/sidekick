25bd4e48df6969a6b317e3a46052f0aecc106eee
Oke Ugwu <oke.ugwu@justdevelop.it>
2013-05-15 13:42:07 +0100

    Proposed Breadcrumb View Model and its application
    Summary: Breadcrumb ViewModel to be used by all Templated ViewModels that have a breadcrumb component. Discuss and improve
    
    Test Plan: TBC
    
    Reviewers: garoevans
    
    Reviewed By: garoevans
    
    Differential Revision: http://phabricator.cubex.io/D17

--- src/Sidekick/Applications/Configurator/Views/ConfigGroupView.php
+++ src/Sidekick/Applications/Configurator/Views/ConfigGroupView.php
@@ -12,26 +12,42 @@ use Sidekick\Components\Projects\Mappers\Project;
 
 class ConfigGroupView extends TemplatedViewModel
 {
-  protected $_form;
   public $configGroups;
   public $project;
   public $parentProject;
 
   public function __construct($projectId)
   {
-    $this->project       = new Project($projectId);
+    $this->project = new Project($projectId);
     $this->parentProject = new Project($this->project->parentId);
-    $this->configGroups  = ConfigurationGroup::collection()->loadWhere(
+    $this->configGroups = ConfigurationGroup::collection()->loadWhere(
       [
       'project_id' => $projectId
       ]
     );
   }
 
+  public function getBreadcrumbs()
+  {
+    $breadcrumbs = new Breadcrumbs();
+    $breadcrumbs->addItem('All Projects', $this->baseUri());
+    if($this->parentProject->exists())
+    {
+      $breadcrumbs->addItem(
+        $this->parentProject->name,
+        $this->baseUri() . '/project/' . $this->parentProject->id()
+      );
+    }
+    $breadcrumbs->addItem(
+      $this->project->name . ' <span class="muted">Config Groups</span>'
+    );
+    return $breadcrumbs;
+  }
+
   public function form()
   {
-    $this->_form = new ConfigGroup($this->baseUri() . "/adding-config-group");
-    $this->_form->addHiddenElement('projectId', $this->project->id());
-    return $this->_form;
+    $form = new ConfigGroup($this->baseUri() . "/adding-config-group");
+    $form->addHiddenElement('projectId', $this->project->id());
+    return $form;
   }
 }

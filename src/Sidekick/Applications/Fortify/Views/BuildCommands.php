62123a6aae5877e18d3939205a4557577eb67116
Oke Ugwu <oke.ugwu@justdevelop.it>
2013-06-10 10:02:29 +0100

    Updated so build commands are ordered by dependencies and to display error message when it encounters an impossible set of dependencies

--- src/Sidekick/Applications/Fortify/Views/BuildCommands.php
+++ src/Sidekick/Applications/Fortify/Views/BuildCommands.php
@@ -9,22 +9,41 @@
 
 namespace Sidekick\Applications\Fortify\Views;
 
+use Cubex\Helpers\DependencyArray;
 use Cubex\View\RenderGroup;
 use Cubex\View\TemplatedViewModel;
+use Sidekick\Applications\BaseApp\Views\Alert;
 use Sidekick\Components\Fortify\Mappers\Command;
 
 class BuildCommands extends TemplatedViewModel
 {
   protected $_buildCommands;
+  protected $_alert;
 
   public function __construct($buildCommands)
   {
     $this->_buildCommands = $buildCommands;
+    try
+    {
+      $this->orderByDependencies();
+    }
+    catch(\Exception $e)
+    {
+      $this->_alert = new Alert(
+        Alert::TYPE_ERROR,
+        'You have some impossible set of dependencies'
+      );
+    }
   }
 
   public function getBuildCommands()
   {
     return $this->_buildCommands;
+  }
+
+  public function getAlert()
+  {
+    return $this->_alert;
   }
 
   public function printDependencies($dependencies = [])
@@ -36,11 +55,37 @@ class BuildCommands extends TemplatedViewModel
 
       $output = new RenderGroup(
         '<ul><li>',
-        implode('</li><li>',$c->getUniqueField("name")),
+        implode('</li><li>', $c->getUniqueField("name")),
         '</li></ul>'
       );
     }
 
     return $output;
+  }
+
+
+  public function orderByDependencies()
+  {
+    $dependencies = new DependencyArray();
+    $rebuild      = []; //lookup array to be used to rebuild $_buildCommands
+    foreach($this->getBuildCommands() as $com)
+    {
+      /**
+       * @var $com BuildsCommands
+       */
+      $dependencies->add(
+        $com->commandId,
+        $com->dependencies
+      );
+
+      $rebuild[$com->commandId] = $com;
+    }
+
+    $orderedCommandList   = $dependencies->getLoadOrder();
+    $this->_buildCommands = []; //reset to rebuild
+    foreach($orderedCommandList as $commandId)
+    {
+      $this->_buildCommands[] = $rebuild[$commandId];
+    }
   }
 }

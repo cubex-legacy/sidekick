30624a2bbd22fcf12a1a5e3131289d7ea14fc2f9
Oke Ugwu <oke.ugwu@justdevelop.it>
2013-06-11 17:32:57 +0100

    added method to return source mapper for build project. Will come in handy when iterating over BuildsProjects objects

--- src/Sidekick/Components/Fortify/Mappers/BuildsProjects.php
+++ src/Sidekick/Components/Fortify/Mappers/BuildsProjects.php
@@ -7,6 +7,7 @@ namespace Sidekick\Components\Fortify\Mappers;
 
 use Cubex\Mapper\Database\PivotMapper;
 use Sidekick\Components\Projects\Mappers\Project;
+use Sidekick\Components\Repository\Mappers\Source;
 
 class BuildsProjects extends PivotMapper
 {
@@ -16,5 +17,10 @@ class BuildsProjects extends PivotMapper
   protected function _configure()
   {
     $this->pivotOn(new Build(), new Project());
+  }
+
+  public function buildSource()
+  {
+    return new Source($this->buildSourceId);
   }
 }

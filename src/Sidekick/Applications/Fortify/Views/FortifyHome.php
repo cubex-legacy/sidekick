50b1152a2f070f0d87e847c86b0319268701f448
Oke Ugwu <oke.ugwu@justdevelop.it>
2013-06-27 18:36:26 +0100

    UI Tweak to fortify homepage. Operation "save screen space"

--- src/Sidekick/Applications/Fortify/Views/FortifyHome.php
+++ src/Sidekick/Applications/Fortify/Views/FortifyHome.php
@@ -6,19 +6,10 @@
 
 namespace Sidekick\Applications\Fortify\Views;
 
-use Cubex\View\TemplatedViewModel;
 use Sidekick\Components\Projects\Mappers\Project;
 
-class FortifyHome extends TemplatedViewModel
+class FortifyHome extends BuildRunsList
 {
-  public $builds;
-
-  public function __construct($builds)
-  {
-    $this->builds = $builds;
-
-  }
-
   public function project($project_id)
   {
     return new Project($project_id);

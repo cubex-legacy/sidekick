9c12f77d726e76c6f565f3ceb39228568004ff39
Oke Ugwu <oke.ugwu@justdevelop.it>
2013-06-28 17:46:13 +0100

    updated fortify home page to show build type instead of run type

--- src/Sidekick/Applications/Fortify/Views/FortifyHome.php
+++ src/Sidekick/Applications/Fortify/Views/FortifyHome.php
@@ -6,12 +6,18 @@
 
 namespace Sidekick\Applications\Fortify\Views;
 
+use Sidekick\Components\Fortify\Mappers\Build;
 use Sidekick\Components\Projects\Mappers\Project;
 
 class FortifyHome extends BuildRunsList
 {
-  public function project($project_id)
+  public function project($projectId)
   {
-    return new Project($project_id);
+    return new Project($projectId);
+  }
+
+  public function build($buildId)
+  {
+    return new Build($buildId);
   }
 }

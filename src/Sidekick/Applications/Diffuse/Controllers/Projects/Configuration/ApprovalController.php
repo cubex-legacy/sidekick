f78f6df8a64c080ace0427b98cf179fab6de9efe
Brooke Bryan <brooke@justdevelop.it>
2013-08-27 12:22:14 +0100

    Approval configurations

--- src/Sidekick/Applications/Diffuse/Controllers/Projects/Configuration/ApprovalController.php
+++ src/Sidekick/Applications/Diffuse/Controllers/Projects/Configuration/ApprovalController.php
@@ -5,14 +5,59 @@
 
 namespace Sidekick\Applications\Diffuse\Controllers\Projects\Configuration;
 
+use Cubex\Data\Transportable\TransportMessage;
+use Cubex\Facade\Redirect;
+use Cubex\Form\Form;
 use Sidekick\Applications\Diffuse\Controllers\DiffuseController;
 use
 Sidekick\Applications\Diffuse\Views\Projects\Configuration\ApprovalConfigurationView;
+use Sidekick\Components\Diffuse\Mappers\ApprovalConfiguration;
+use Sidekick\Components\Diffuse\Mappers\Platform;
+use Sidekick\Components\Projects\Mappers\Project;
+use Sidekick\Components\Projects\Mappers\ProjectUser;
+use Sidekick\Components\Sidekick\Enums\Consistency;
 
 class ApprovalController extends DiffuseController
 {
   public function renderIndex()
   {
-    return new ApprovalConfigurationView();
+    $project   = new Project($this->getInt("projectId"));
+    $platforms = Platform::orderedCollection();
+    $approvals = ApprovalConfiguration::collection(
+      ['project_id' => $project->id()]
+    );
+    $users     = ProjectUser::collection(['project_id' => $project->id()]);
+    return new ApprovalConfigurationView($project, $approvals, $platforms, $users);
+  }
+
+  public function postIndex()
+  {
+    if($this->request()->isForm() && Form::csrfCheck())
+    {
+      foreach($this->postVariables() as $platRole => $consistency)
+      {
+        list($platform, $role) = explode('-', $platRole, 2);
+
+        $id = [
+          $platform,
+          $this->getInt("projectId"),
+          $role
+        ];
+
+        $approval                   = new ApprovalConfiguration($id);
+        $approval->consistencyLevel = Consistency::fromValue($consistency);
+        $approval->required         = true;
+        if($approval->consistencyLevel == Consistency::NONE)
+        {
+          $approval->delete();
+        }
+        else
+        {
+          $approval->saveChanges();
+        }
+      }
+    }
+    $msg = new TransportMessage("success", "Approval configuration update");
+    return Redirect::to($this->request()->path(3))->with("msg", $msg);
   }
 }
--- src/Sidekick/Applications/Diffuse/Views/Projects/Configuration/ApprovalConfigurationView.php
+++ src/Sidekick/Applications/Diffuse/Views/Projects/Configuration/ApprovalConfigurationView.php
@@ -5,8 +5,109 @@
 
 namespace Sidekick\Applications\Diffuse\Views\Projects\Configuration;
 
+use Cubex\Form\Form;
+use Cubex\Mapper\Database\RecordCollection;
 use Cubex\View\TemplatedViewModel;
+use Sidekick\Components\Diffuse\Mappers\ApprovalConfiguration;
+use Sidekick\Components\Diffuse\Mappers\Platform;
+use Sidekick\Components\Projects\Mappers\Project;
+use Sidekick\Components\Projects\Mappers\ProjectUser;
 
 class ApprovalConfigurationView extends TemplatedViewModel
 {
+  protected $_project;
+  protected $_approvals;
+  protected $_platforms;
+  protected $_users;
+  protected $_form;
+
+  /**
+   * @param Project                                  $project
+   * @param RecordCollection|ApprovalConfiguration[] $approvalConfigs
+   * @param RecordCollection|Platform[]              $platforms
+   * @param RecordCollection|ProjectUser[]           $users
+   */
+  public function __construct(
+    Project $project, $approvalConfigs, $platforms, $users
+  )
+  {
+    $this->_project   = $project;
+    $this->_approvals = $approvalConfigs;
+    $this->_platforms = $platforms;
+    $this->_users     = $users;
+    $roles            = [];
+    foreach($users as $user)
+    {
+      foreach($user->roles as $role)
+      {
+        $roles[$role]++;
+      }
+    }
+    $this->_roles = $roles;
+    $form         = new Form('ApprovalConfigForm');
+
+    foreach($platforms as $platform)
+    {
+      foreach($roles as $role => $roleCount)
+      {
+        $options = ['none' => 'None (0)'];
+        if($roleCount > 0)
+        {
+          $options['one'] = 'One (1)';
+        }
+        if($roleCount > 1)
+        {
+          $options['two'] = 'Two (2)';
+        }
+        $options['quorum'] = 'Quorum ' . (ceil($roleCount / 2) + 1) . ')';
+        $options['all']    = 'All (' . $roleCount . ')';
+
+        $current = new ApprovalConfiguration(
+          [
+          $platform->id(),
+          $project->id(),
+          $role
+          ]
+        );
+
+        $form->addSelectElement(
+          $platform->id() . '-' . $role,
+          $options,
+          $current->consistencyLevel,
+          Form::LABEL_NONE
+        );
+      }
+    }
+    $this->_form = $form;
+  }
+
+  public function project()
+  {
+    return $this->_project;
+  }
+
+  public function approvals()
+  {
+    return $this->_approvals;
+  }
+
+  public function platforms()
+  {
+    return $this->_platforms;
+  }
+
+  public function users()
+  {
+    return $this->_users;
+  }
+
+  public function roles()
+  {
+    return $this->_roles;
+  }
+
+  public function form()
+  {
+    return $this->_form;
+  }
 }

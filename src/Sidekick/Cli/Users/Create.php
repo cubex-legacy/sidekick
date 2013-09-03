436493513d2f42f9901b97784c37730ba743ad28
Oke Ugwu <oke.ugwu@justdevelop.it>
2013-09-03 11:08:16 +0100

    Simple Cli script for creating user accounts. Fixes T178

---
+++ src/Sidekick/Cli/Users/Create.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Author: oke.ugwu
+ * Date: 03/09/13 10:34
+ */
+namespace Sidekick\Cli\Users;
+
+use Cubex\Cli\CliCommand;
+use Cubex\Cli\UserPrompt;
+use Sidekick\Components\Users\Mappers\User;
+
+class Create extends CliCommand
+{
+  /**
+   * @required
+   * @valuerequired
+   */
+  public $username;
+  /**
+   * @required
+   * @valuerequired
+   */
+  public $password;
+
+  public function execute()
+  {
+    $user = User::collection(['username' => $this->username])->first();
+    if($user->exists())
+    {
+      $updatePassword = UserPrompt::confirm(
+        "$this->username already exists. Do you wish to update password?"
+      );
+      if(!$updatePassword)
+      {
+        exit;
+      }
+      else
+      {
+        $user->password = password_hash($this->password, PASSWORD_DEFAULT);
+        $user->saveChanges();
+        echo "Password updated!".PHP_EOL;
+      }
+    }
+    else
+    {
+      $user = new User();
+      $user->username = $this->username;
+      $user->password = password_hash($this->password, PASSWORD_DEFAULT);
+      $user->saveChanges();
+      echo "User Account created!".PHP_EOL;
+    }
+  }
+}

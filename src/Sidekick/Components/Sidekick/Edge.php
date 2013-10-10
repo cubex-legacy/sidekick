b605e18d7fda6bc603f4273585e40fc88b74752b
Brooke Bryan <brooke@justdevelop.it>
2013-10-10 15:53:50 +0100

    Edge container

---
+++ src/Sidekick/Components/Sidekick/Edge.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author  brooke.bryan
+ */
+
+namespace Sidekick\Components\Sidekick;
+
+use Cubex\Mapper\Database\EdgeRecordMapper;
+
+/**
+ * Sidekick Mapper Edges
+ */
+class Edge extends EdgeRecordMapper
+{
+}

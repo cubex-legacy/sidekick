490069e7c378fcc3a15fbdb0173c64e8c2192992
Brooke Bryan <brooke@bajb.net>
2014-01-25 15:19:59 +0000

    Track TODOs in repository

--- src/Sidekick/Components/Fortify/Analysers/PhpSuperGlobals/PhpSuperGlobals.php
+++ src/Sidekick/Components/Fortify/Analysers/PhpSuperGlobals/PhpSuperGlobals.php
@@ -3,6 +3,7 @@ namespace Sidekick\Components\Fortify\Analysers\PhpSuperGlobals;
 
 use Sidekick\Components\Fortify\Analysers\AbstractAnalyser;
 use Sidekick\Components\Repository\Mappers\Commit;
+use Symfony\Component\Process\Process;
 
 class PhpSuperGlobals extends AbstractAnalyser
 {
@@ -13,7 +14,11 @@ class PhpSuperGlobals extends AbstractAnalyser
 
   public function configure($configuration)
   {
-    if(!is_array($configuration))
+    if($configuration === null)
+    {
+      return;
+    }
+    else if(!is_array($configuration))
     {
       $this->_pattern = $configuration;
     }
@@ -25,20 +30,38 @@ class PhpSuperGlobals extends AbstractAnalyser
 
   public function analyse(Commit $commit)
   {
-    //TODO: Add all todo's to a new Table for quick lookups and listing
-    //Remove all dropped todos from the data source - list the commit and date
+    $passed       = true;
+    $locatedTodos = [];
+    $todos        = 0;
 
-    $passed = true;
-
-    foreach($this->_getCurrentFiles($commit) as $file)
+    foreach($this->_getRepositoryFiles() as $file)
     {
-      if(preg_match("/$this->_pattern/", $file->filePath))
+      if(preg_match("/$this->_pattern/", $file))
       {
-        echo $file->filePath . "\n";
+        foreach($this->_matchOn as $match)
+        {
+          $process = new Process("grep -i -n '$match' $file");
+          $process->run();
+          if($process->getExitCode() === 0)
+          {
+            $fileName = substr($file, strlen($this->_basePath) + 1);
+
+            $matches = phutil_split_lines($process->getOutput(), false);
+            foreach($matches as $line)
+            {
+              list($lineNumber, $todo) = explode(':', $line, 2);
+              if($lineNumber > 0)
+              {
+                $locatedTodos[$fileName][$lineNumber] = trim($todo);
+                $todos++;
+              }
+            }
+          }
+        }
       }
     }
-
-    $this->_trackInsight("todos", 6);
+    $this->_storeData("todos.json", json_encode($locatedTodos));
+    $this->_trackInsight('todos', $todos);
 
     return $passed;
   }

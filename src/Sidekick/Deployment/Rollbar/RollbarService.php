f2e385f49d509652b934e9f81342c33e30c1cf37
Brooke Bryan <brooke@bajb.net>
2014-04-03 11:32:37 +0100

    Rollbar Service

---
+++ src/Sidekick/Deployment/Rollbar/RollbarService.php
@@ -0,0 +1,40 @@
+<?php
+namespace Sidekick\Deployment\Rollbar;
+
+use Cubex\Data\Handler\DataHandler;
+use Cubex\Log\Log;
+use Sidekick\Deployment\BaseDeploymentService;
+
+class RollbarService extends BaseDeploymentService
+{
+  /**
+   * @return void
+   */
+  public function deploy()
+  {
+    $cfg = (new DataHandler())->hydrate($this->_stage->configuration);
+    \Requests::post(
+      'https://api.rollbar.com/api/1/deploy/',
+      [],
+      [
+        'access_token' => $cfg->getStr("command", null),
+        'environment'  => $this->_stage->platform()->name,
+        'revision'     => $this->_version->format(),
+        'comment'      => $this->_version->changeLog
+      ]
+    );
+
+    Log::info(
+      'https://api.rollbar.com/api/1/deploy/' . $this->_version->format(
+      ) . '/' . $this->_stage->platform()->name
+    );
+  }
+
+  public static function getConfigurationItems()
+  {
+    return [
+      'access_token' => 'Your project access token. Required.',
+    ];
+  }
+}
+

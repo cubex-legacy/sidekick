be2355302b123db7726fd6255eb9cf843ca17504
Oke Ugwu <oke.ugwu@justdevelop.it>
2015-11-06 13:28:32 +0000

    improved how we retry changes between builds to branch specific

--- src/Sidekick/Components/Fortify/FortifyBuildChanges.php
+++ src/Sidekick/Components/Fortify/FortifyBuildChanges.php
@@ -8,7 +8,9 @@ namespace Sidekick\Components\Fortify;
 
 use Sidekick\Components\Fortify\Enums\BuildResult;
 use Sidekick\Components\Fortify\Mappers\BuildRun;
+use Sidekick\Components\Repository\Mappers\Branch;
 use Sidekick\Components\Repository\Mappers\Commit;
+use Sidekick\Components\Repository\Mappers\Source;
 
 class FortifyBuildChanges
 {
@@ -41,15 +43,40 @@ class FortifyBuildChanges
     {
       $lastCommitHash->whereLessThan("id", $this->runId);
     }
-    $lastCommitHash = $lastCommitHash
-    ->setOrderBy("created_at", 'DESC')
-    ->setColumns(['commit_hash'])
-    ->setLimit(0, 1)
-    ->first();
 
-    return Commit::collectionBetween(
-      $this->_commitHash,
-      idp($lastCommitHash, "commitHash")
-    );
+    $range = array();
+    $repo = Source::loadWhere(["project_id" => $this->projectId]);
+    if($repo)
+    {
+      $branch   = Branch::loadWhere(["repository_id" => $repo->id()]);
+      $branchId = $branch->id();
+
+      $lastCommitHash = $lastCommitHash
+        ->setOrderBy("created_at", 'DESC')
+        ->setColumns(['commit_hash'])
+        ->setLimit(0, 1)
+        ->first();
+
+      $findCommits = [
+        $this->_commitHash,
+        idp($lastCommitHash, "commitHash")
+      ];
+
+      $commitIds = Commit::collection()
+        ->whereIn('commit_hash', $findCommits)
+        ->whereEq('branch_id', $branchId)
+        ->setColumns(['commit_hash', 'branch_id', 'id'])
+        ->get();
+
+      $range = Commit::collection();
+      if(count($commitIds->loadedIds()) == 2)
+      {
+        $range->whereBetween("id", $commitIds->loadedIds());
+      }
+      $range->whereEq("branch_id", $branchId)
+        ->setOrderBy('committed_at', 'DESC');
+    }
+
+    return $range;
   }
 }

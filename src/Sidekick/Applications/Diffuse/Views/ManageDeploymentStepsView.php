11b248860bcbb1a6c1dbb3e80fab336335c8289f
Oke Ugwu <oke.ugwu@justdevelop.it>
2016-05-09 10:44:32 +0100

    set platform id on edit form

--- src/Sidekick/Applications/Diffuse/Views/ManageDeploymentStepsView.php
+++ src/Sidekick/Applications/Diffuse/Views/ManageDeploymentStepsView.php
@@ -9,16 +9,16 @@
 
 namespace Sidekick\Applications\Diffuse\Views;
 
-use Cubex\Form\Form;
 use Cubex\Form\FormElement;
-use Cubex\Foundation\Container;
 use Cubex\View\TemplatedViewModel;
 use Sidekick\Applications\Diffuse\Forms\DeploymentStageForm;
-use Sidekick\Components\Diffuse\Helpers\DeploymentHelper;
-use Sidekick\Components\Projects\Mappers\Project;
+use Sidekick\Components\Diffuse\Mappers\DeploymentStep;
 
 class ManageDeploymentStepsView extends TemplatedViewModel
 {
+  /**
+   * @var DeploymentStep $_step
+   */
   protected $_step;
   public $configId;
 
@@ -43,6 +43,7 @@ class ManageDeploymentStepsView extends TemplatedViewModel
     if($this->_step->exists())
     {
       $buttonText = 'Update';
+      $form->platformId = $this->_step->platformId;
       $form->getElement('platformId')->setType(FormElement::HIDDEN);
     }
 

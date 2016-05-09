24e1584191fd82478774d7410cd35b511de23bae
Oke Ugwu <oke.ugwu@justdevelop.it>
2016-05-09 11:02:23 +0100

    simplified header nav. stripped out unused code

--- src/Sidekick/Applications/BaseApp/Views/Header.php
+++ src/Sidekick/Applications/BaseApp/Views/Header.php
@@ -30,50 +30,21 @@ class Header extends ViewModel
 
   public function render()
   {
-    $structure = [];
-
-    /**
-     * @var $apps \Sidekick\Applications\BaseApp\SidekickApplication[]
-     */
-    $apps = $this->_project->getApplications();
-    foreach($apps as $appPath => $app)
-    {
-      if(!$app->userPermitted(Auth::user()->getDetail('user_role')))
-      {
-        continue;
-      }
-
-      $group = null;
-      if(method_exists($app, "getNavGroup"))
-      {
-        $group = $app->getNavGroup();
-      }
-      if($group !== null)
-      {
-        $structure[$group][$appPath] = $app;
-      }
-      else
-      {
-        $structure[$app->name()][$appPath] = $app;
-      }
-    }
-
     $navItems = new Partial(
       '<li class="%s"><a %s class="%s" href="%s" title="%s">%s</a>%s</li>',
       null, false
     );
 
-    $subNavItems = new Partial(
-      '<li class="%s">
-        <a data-animation="true" data-placement="bottom" href="%s" title="%s">
-          %s
-        </a>
-      </li>'
+    $navItems->addElement(
+      '',
+      '',
+      '',
+      '/projects',
+      'New menu',
+      'Projects',
+      ''
     );
 
-    $path = $this->request()->path();
-
-    ksort($structure);
     $navItems->addElement(
       '',
       '',
@@ -91,16 +62,6 @@ class Header extends ViewModel
       '/fortify/build-configs',
       'New menu',
       'Build Configuration',
-      ''
-    );
-
-    $navItems->addElement(
-      '',
-      '',
-      '',
-      '/projects',
-      'New menu',
-      'Projects',
       ''
     );
 
@@ -143,59 +104,6 @@ class Header extends ViewModel
       ''
     );
 
-    /*foreach($structure as $group => $apps)
-    {
-      ksort($structure[$group]);
-    }
-
-    foreach($structure as $group => $apps)
-    {
-      if(count($apps) > 1)
-      {
-        foreach($apps as $appPath => $app)
-        {
-          if($app instanceof Application)
-          {
-            //active if path starts with appPath
-            $state = $this->_getSubNavState($path, $appPath);
-            $subNavItems->addElement(
-              $state,
-              ('/' . $appPath),
-              $app->description(),
-              $app->name()
-            );
-          }
-        }
-
-        $state = $this->_getMainNavState($path, $structure[$group]);
-        $navItems->addElement(
-          'dropdown' . ' ' . $state,
-          'data-toggle="dropdown" data-hover="dropdown"',
-          'dropdown-toggle',
-          ('/' . $appPath),
-          '',
-          ($group . ' <b class="caret"></b>'),
-          ('<ul class="dropdown-menu">' . $subNavItems->render() . '</ul>')
-        );
-
-        $subNavItems->clearElements();
-      }
-      else
-      {
-        $state   = $this->_getMainNavState($path, $structure[$group]);
-        $appPath = key($apps);
-        $navItems->addElement(
-          $state,
-          '',
-          '',
-          ('/' . $appPath),
-          $apps[$appPath]->description(),
-          $group,
-          ''
-        );
-      }
-    }*/
-
     return new RenderGroup(
       '<a id="sidekick-logo" class="brand" href="/">',
       ('<img src="' . $this->imgUrl('/logo.png') . '"/></a>'),
@@ -213,27 +121,4 @@ class Header extends ViewModel
     );
   }
 
-  private function _getMainNavState($path, $navGroup)
-  {
-    $currentApp = explode('/', $path)[1];
-    $state      = '';
-    if(array_key_exists($currentApp, $navGroup))
-    {
-      $state = 'active';
-    }
-
-    return $state;
-  }
-
-  /**
-   * @param $path
-   * @param $appPath
-   *
-   * @return string
-   */
-  private function _getSubNavState($path, $appPath)
-  {
-    $state = starts_with($path, "/$appPath", false) ? ' active' : '';
-    return $state;
-  }
 }
